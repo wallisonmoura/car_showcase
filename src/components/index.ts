3370bda784ab59c463dfcce8a6897e6e9ae667a9
wallisonmoura <wallison_cedaspy@hotmail.com>
2023-07-03 17:36:51 -0300

    feat: Creation of car cards and connection to the rapid api to search for cars.

--- src/components/index.ts
+++ src/components/index.ts
@@ -5,6 +5,7 @@ import Footer from './Footer'
 import SearchBar from './SearchBar'
 import CustomFilter from './CustomFilter'
 import SearchManuFacturer from './SearchManuFacturer'
+import CarCard from './CarCard'
 
 export {
   Hero,
@@ -13,5 +14,6 @@ export {
   SearchBar,
   CustomFilter,
   SearchManuFacturer,
+  CarCard,
   Footer
 }
--- src/types/index.ts
+++ src/types/index.ts
@@ -3,11 +3,29 @@ import { MouseEventHandler } from "react"
 export type CustomButtonProps = {
   title: string
   containerStyles?: string
+  textStyles?: string
   btnType?: 'button' | 'submit'
   handleClick?: MouseEventHandler<HTMLButtonElement>
+  rightIcon?: string
+  isDisabled?: boolean
 }
 
 export type SearchManuFacturerProps = {
   manuFacturer: string
   setManuFacturer: (manuFacturer: string) => void
+}
+
+export type CarProps = {
+  city_mpg: number
+  class: string
+  combination_mpg: number
+  cylinders: number
+  displacement: number
+  drive: string
+  fuel_type: string
+  highway_mpg: number
+  make: string
+  model: string
+  transmission: string
+  year: number
 }
